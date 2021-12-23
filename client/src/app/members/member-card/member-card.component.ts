e6da6322d4cbee4cd3c5fb2ce9cbd2fc43541e12
nruizjr <njruiz.nr@gmail.com>
2021-12-23 14:04:51 +0800

    Partial Implementation of Image Upload
    Until Lesson 135 only

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -3,7 +3,7 @@ import { Member } from 'src/app/_models/member';
 
 @Component({
   selector: 'app-member-card',
-  templateUrl: './member-card.component.html',
+  templateUrl: "./member-card.component.html",
   styleUrls: ['./member-card.component.css'],
 })
 export class MemberCardComponent implements OnInit {
