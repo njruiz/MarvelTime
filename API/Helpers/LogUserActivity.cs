1802d1723a15b16a01c3af4ef6bb3349a1ab45e4
Nelson Ruiz <njruiz.nr@gmail.com>
2022-01-14 18:16:19 +0800

    Implement Unit of Work pattern
    - Transactional approach
    - Optimized queries to the DB
    - Added a confirm dialog
    - Added scrolling in messages

--- API/Helpers/LogUserActivity.cs
+++ API/Helpers/LogUserActivity.cs
@@ -7,16 +7,16 @@ namespace API.Helpers
     public class LogUserActivity : IAsyncActionFilter
     {
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
-        {            
+        {
             var resultContext = await next();
 
             if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
 
             var userId = resultContext.HttpContext.User.GetUserId();
-            var repo = resultContext.HttpContext.RequestServices.GetService<IUserRepository>();
-            var user = await repo.GetUserByIdAsync(userId);
+            var uow = resultContext.HttpContext.RequestServices.GetService<IUnitOfWork>();
+            var user = await uow.UserRepository.GetUserByIdAsync(userId);
             user.LastActive = DateTime.UtcNow;
-            await repo.SaveAllAsync();
+            await uow.Complete();
         }
     }
 }
